e708c269ff2a431142be71844dac55cbcab872b6
aganonki <aganonki@gmail.com>
2015-06-07 13:13:00 +0300

    Added JsonConfigHelper which inherits ConfigUtils
    Added not static JsonConfigHelper class which inherits ConfigUtils

--- ExternalUtilsCSharp/JSONConfigUtils.cs
+++ ExternalUtilsCSharp/JSONConfigUtils.cs
@@ -42,6 +42,64 @@ namespace ExternalUtilsCSharp
         }
 
     }
+    #region not static JsonConfigs
+
+    /// <summary>
+    /// Json configs helper which Deserialize Objects from file
+    /// </summary>
+    /// <typeparam name="T">T is settings Object</typeparam>
+    public class JsonConfigHelper<T> : ConfigUtils where T : new()
+    {
+        public T Settings = new T();
+        private string lastConfigName;
+        public JsonConfigHelper() { }
+        /// <summary>
+        /// Created default config to specified path
+        /// </summary>
+        public virtual void DefaultConfig(string configName)
+        {
+            if (File.Exists(configName)) return;
+            JSONConfigManager.WriteFile(Settings, configName);
+        }
+        /// <summary>
+        ///  Use ReadSettingsFromFile(string) method THIS one is not implemented
+        ///  since byte[] data is not required
+        /// </summary>
+        public override void ReadSettings(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Saves Setings object to latest used configuration
+        /// </summary>
+        /// <returns>return is not required</returns>
+        public override byte[] SaveSettings()
+        {
+            JSONConfigManager.WriteFile(Settings, lastConfigName);
+            return null;
+        }
+        /// <summary>
+        /// Reads settings from file and stores it into Settings Object
+        /// </summary>
+        /// <param name="configName">Path of file</param>
+        public override void ReadSettingsFromFile(string configName) 
+        {
+            lastConfigName = configName;
+            Settings = JSONConfigManager.ReadFile<T>(configName);
+        }
+        /// <summary>
+        /// Saves Settings object to specified file
+        /// </summary>
+        /// <param name="configFileName">Path of file</param>
+        public override void SaveSettingsToFile(string configFileName)
+        {
+            lastConfigName = configFileName;
+            JSONConfigManager.WriteFile(Settings, configFileName);
+        }
+
+    }
+    #endregion
+
     public static class JSONConfigManager
     {
         public static T ReadFile<T>(string filename) where T : new()
@@ -60,8 +118,9 @@ namespace ExternalUtilsCSharp
             }
             catch (Exception e)
             {
-                data = null;
-                return new T();
+                throw new Exception("Could not read settings\n",e);
+                //data = null;
+                //return new T();
             }
         }
         public static void WriteFile<T>(T objToWrite, string fileName)
